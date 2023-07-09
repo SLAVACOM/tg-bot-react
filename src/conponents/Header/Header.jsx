3df60546abdc510f75903d879450a76a17c2cd05
SLAVACOM <slavacom121@gmai.com>
2023-07-09 22:36:08 +0300

    Initialize project using Create React App

---
+++ src/conponents/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "../button/Button";
+
+const Header = () =>{
+    const tg = <window className="Telegram WebApp"></window>
+    const onClose = ()=>{
+        tg.close()
+    }
+     return(
+         <div className={'header'}>
+             <Button onClick={onClose}>Закрыть</Button>
+             <span className={'username'}>
+                 {tg.initDataUnsafe?.user?.username}
+             </span>
+         </div>
+     )
+}
+export default Header
