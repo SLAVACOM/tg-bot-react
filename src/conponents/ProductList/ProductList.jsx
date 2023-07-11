63edc392c1c15e98d32061af12544a6541c393ca
SLAVACOM <slavacom121@gmai.com>
2023-07-11 14:10:21 +0300

    server

--- src/conponents/ProductList/ProductList.jsx
+++ src/conponents/ProductList/ProductList.jsx
@@ -3,6 +3,7 @@ import './ProductList.css'
 import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback,useEffect} from "react";
+const p=''
 const products = [
     {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
     {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
